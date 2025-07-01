cca2e616a46f7f6094ac2b7ec5e6ff991593a93e
kwp@tlen.pl <kwp@tlen.pl>
2025-07-01 15:16:38 +0200

    zmiany do 15:00

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
 
 from .models import FavouritePost, Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list,3)
+    page_number = request.GET.get('page',1)
+    posts = paginator.page(page_number)
     return render(
         request,
         'blog/post/list.html',
