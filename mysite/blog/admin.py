ac0232db32eb3c48dc752406aed14f5ff8e893db
kwp@tlen.pl <kwp@tlen.pl>
2025-07-01 13:36:21 +0200

    zmiany do 13:30

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,11 +1,39 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.auth.models import User
 
 from .models import Post
 
+# napisać klasę, która zmieni formularz w User - czyli pole wyszukiwania ustawi na email
+
+
+
+class PostAdminForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
+        widgets = {
+            'body': forms.Textarea(attrs={
+                'rows': 10,                    # Wysokość: 10 wierszy
+                'cols': 50,                    # Szerokość: ~50 znaków
+                'placeholder': 'Wpisz treść posta...',  # Tekst zastępczy
+                'class': 'form-control',       # Klasa CSS (np. Bootstrap)
+                'style': 'resize: vertical;',  # Styl CSS (tylko pionowy resize)
+                'maxlength': 1000,             # Maks. 1000 znaków
+                'required': True,              # Pole wymagane
+                'id': 'post-body',             # Unikalne ID
+                'data-max-chars': '1000',      # Dane dla JavaScript
+            })
+        }
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
     list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_display_links = ['title', 'slug']
+    list_editable = ['status']
+    list_per_page = 25
+    autocomplete_fields = ['author']
     list_filter = ['status', 'created', 'publish', 'author']
     search_fields = ['title', 'body']
     prepopulated_fields = {'slug': ('title',)}
@@ -13,3 +41,21 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
     show_facets = admin.ShowFacets.ALWAYS
+    search_help_text = "Wyszukaj posty po tytule lub treści"
+
+# żeby zarejestować nowe zmiany, należy wyrejestrować Django'wy model
+# admin.site.unregister(User)
+#
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     search_fields = ['email']
+
+
+
+    # Wyświetlanie kluczowych informacji w tabeli (list_display).
+    # Filtrowanie i wyszukiwanie postów (list_filter, search_fields).
+    # Automatyzację tworzenia pól (prepopulated_fields).
+    # Optymalizację dla dużych danych (raw_id_fields).
+    # Łatwą nawigację po datach (date_hierarchy).
+    # Domyślne sortowanie (ordering).
+    # Lepsze filtrowanie dzięki fasetkom (show_facets).
