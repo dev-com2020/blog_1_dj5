98aa2315c701c226e004d78be0717e9aee20c39a
Twoje Imię <twj@email.com>
2025-07-04 09:08:39 +0200

    testowy 2

--- quiz_project/quiz_app/urls.py
+++ quiz_project/quiz_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('history/',TestListView.as_view(), name='history'),
+    path('download/<int:test_id>', download, name='test_download'),
 ]
